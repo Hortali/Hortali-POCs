87edfb45c2919886adaee98a512d39b390b08b9d
Gui Reis <gui.sreis25@gmail.com>
2022-08-24 14:34:22 -0300

    Criando projeto no XCode

---
+++ Gui/POC-Collection/POC-Collection/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  POC-Collection
+//
+//  Created by Gui Reis on 23/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
